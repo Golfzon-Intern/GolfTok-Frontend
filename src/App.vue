9d30432018dbdda93d1981ac247691933c002d20
JoieHuiju <joie.huiju@gmail.com>
2021-07-14 15:32:52 +0900

    [Add] Add video list
    - axios 사용해 요청하고, 받아온 데이터를 활용해 비디오 동영상 생성하도록 함
      (요청 url은 임의로 만든 json 파일로 서버 요청 url로 변경해야 함)
    - video.js 라이브러리 사용

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="video">
+    <h1>This is a video page</h1>
+    <ul v-if="videoOptions" style="list-style:none">
+      <template v-for="(option, index) in videoOptions">
+        <li v-bind:key="index">
+          <video-player :options="option" />
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import VideoPlayer from "@/components/VideoPlayer.vue";
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      videoOptions: [],
+    };
+  },
+  created: function() {
+    this.getVideoInfo();
+  },
+  methods: {
+    getVideoInfo: function() {
+      let temp = [];
+      axios
+        .get(
+          "https://gist.githubusercontent.com/Joie-Kim/90f0e1e50cfa339db6397efe147dce72/raw/419f130d2b101af4613001869e4204d08e2d0b16/sampleVideo.json"
+        )
+        .then(function(response) {
+          const videos = response.data.videos;
+          const resLen = videos.length;
+          if (resLen > 0) {
+            for (let i = 0; i < resLen; i++) {
+              const videoUrl = videos[i].sources[0];
+
+              const videoObj = {
+                autoplay: false,
+                controls: true,
+                sources: [
+                  {
+                    src: videoUrl,
+                    type: "video/mp4",
+                  },
+                ],
+              };
+
+              temp.push(videoObj);
+            }
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+      this.videoOptions = temp;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    "video-player": VideoPlayer,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      this.options,
+      function onPlayerReady() {
+        console.log("onPlayerReady", this);
+      }
+    );
+  },
+  beforeUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+
+<style></style>
