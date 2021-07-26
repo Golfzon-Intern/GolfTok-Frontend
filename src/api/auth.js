e2ee3fe27150f086a9175b1a4cf7afe0519e5583
JoieHuiju <joie.huiju@gmail.com>
2021-07-26 16:14:17 +0900

    [Update] Change api url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,5 @@
 import http from '@/api/http';
 
 export function login(userId, password) {
-  return http.post('/login', { userName: userId, userPassword: password });
+  return http.post('/user/login', { userName: userId, userPassword: password });
 }
