020498ba5d7e984bda6fddb812ab1e75641b8788
JoieHuiju <joie.huiju@gmail.com>
2021-08-13 10:48:45 +0900

    [Update] Fix like component api url

--- src/api/like.js
+++ src/api/like.js
@@ -19,16 +19,16 @@ export function deletePostLiked(targetId) {
 
 // 특정 댓글 좋아요 여부 조회
 export function getCommentLike(targetId) {
-  return http.get('/like/post', { params: { commentId: targetId } });
+  return http.get('/like/comment', { params: { commentId: targetId } });
   // return axios.get('https://gist.githubusercontent.com/Joie-Kim/184928b11f72cea1bdf4388afae13087/raw/66a9f6c63456b0798ff75db89600706ec91a6c3c/dummy_like.json', { params: { commentId: targetId } });
 }
 
 // 특정 댓글 좋아요 추가
 export function addCommentLike(targetId) {
-  return http.post('/like/post', { commentId: targetId });
+  return http.post('/like/comment', { commentId: targetId });
 }
 
 // 특정 댓글 좋아요 삭제
 export function deleteCommentLiked(targetId) {
-  return http.delete('/like/post', { params: { commentId: targetId } });
+  return http.delete('/like/comment', { params: { commentId: targetId } });
 }
