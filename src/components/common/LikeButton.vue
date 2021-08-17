fc54799928937e9c84cf00e8dfd500548b9599e3
JoieHuiju <joie.huiju@gmail.com>
2021-08-17 19:05:02 +0900

    [Etc] Rollback default value of prop data

--- src/components/common/LikeButton.vue
+++ src/components/common/LikeButton.vue
@@ -23,25 +23,25 @@ export default {
       numOfLike: 0,
     };
   },
-  // props: {
-  // targetType: {
-  //   type: String,
-  //   default: '',
-  // },
-  // targetId: {
-  //   type: Number,
-  //   default: 0,
-  // },
-  // styleType: {
-  //   type: Number,
-  //   default: 0,
-  // },
-  // },
   props: {
-    targetType: String,
-    targetId: Number,
-    styleType: Number,
+    targetType: {
+      type: String,
+      default: '',
+    },
+    targetId: {
+      type: Number,
+      default: 0,
+    },
+    styleType: {
+      type: Number,
+      default: 0,
+    },
   },
+  // props: {
+  //   targetType: String,
+  //   targetId: Number,
+  //   styleType: Number,
+  // },
   created() {
     this.getLiked();
     console.log(this.targetType + this.styleType + this.targetId);
