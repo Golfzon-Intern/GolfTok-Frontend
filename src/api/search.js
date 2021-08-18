1faeb5cd5fa0a32213dae4a5187ca64eccda1384
JoieHuiju <joie.huiju@gmail.com>
2021-08-18 19:38:07 +0900

    [Update] Change search api url

--- src/api/search.js
+++ src/api/search.js
@@ -2,6 +2,6 @@
 import http from '@/api/http';
 
 export function getSearchResult(keyword) {
-  return http.get('/search', { params: { keyword: keyword } });
+  return http.get('/post/search', { params: { keyword: keyword } });
   //   return axios.get('https://gist.githubusercontent.com/dbswl4951/8eb63f3dbc2fabc2135fdbeae33df56e/raw/de4bcdc1d1b94c69b49eece2bb89033d707bdb26/dummy_search', { params: { keyword: keyword } });
 }
