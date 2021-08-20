a5122e8ffa01691063830df52bb5dea500b614eb
JoieHuiju <joie.huiju@gmail.com>
2021-08-20 15:22:02 +0900

    [Update] Update location searching api
    - 위치 검색 api 수정

--- src/api/search.js
+++ src/api/search.js
@@ -1,10 +1,10 @@
-// import axios from 'axios';
+import axios from 'axios';
 import http from '@/api/http';
 
 // 키워드 검색
 export function getSearchResult(keyword) {
-  //   return http.get('/post/search', { params: { keyword: keyword } });
-  return axios.get('https://gist.githubusercontent.com/dbswl4951/8eb63f3dbc2fabc2135fdbeae33df56e/raw/de4bcdc1d1b94c69b49eece2bb89033d707bdb26/dummy_search', { params: { keyword: keyword } });
+  return http.get('/post/search', { params: { keyword: keyword } });
+  // return axios.get('https://gist.githubusercontent.com/dbswl4951/8eb63f3dbc2fabc2135fdbeae33df56e/raw/de4bcdc1d1b94c69b49eece2bb89033d707bdb26/dummy_search', { params: { keyword: keyword } });
 }
 
 // 위치 검색
