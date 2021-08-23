af6c94968ddf2b0086af9f4a893d6b97b987f3fd
JoieHuiju <joie.huiju@gmail.com>
2021-08-23 13:40:13 +0900

    [Update] Update follow api

--- src/api/follow.js
+++ src/api/follow.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+// import axios from 'axios';
 import http from '@/api/http';
 
 export function getFollowing(targetId) {
-  //   return http.get('/user/following', { params: { friendId: targetId } });
-  return axios.get('https://gist.githubusercontent.com/Joie-Kim/566e03c8b5e451c1895074ff0a200f1a/raw/9ceea4e07461a2ed286c46a208d3a8d86ee0c321/dummy_getFollowing.json', {
-    params: { friendId: targetId },
-  });
+  return http.get('/user/following', { params: { friendId: targetId } });
+  // return axios.get('https://gist.githubusercontent.com/Joie-Kim/566e03c8b5e451c1895074ff0a200f1a/raw/9ceea4e07461a2ed286c46a208d3a8d86ee0c321/dummy_getFollowing.json', {
+  //   params: { friendId: targetId },
+  // });
 }
 
 export function addFollowing(targetId) {
