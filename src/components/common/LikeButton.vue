809b4570fcaab62f787a0ebfc3d01ede925b6282
김희주/모바일개발팀 <huijoo5575@golfzon.com>
2021-08-30 12:02:05 +0900

    [Update] Open login modal in post list
    - 로그인 정보가 없을 때, 좋아요 버튼이나 영상을 누르면 로그인 모달이 열리도록 수정

--- src/components/common/LikeButton.vue
+++ src/components/common/LikeButton.vue
@@ -2,7 +2,10 @@
   <div class="like-wrapper" :style="boxStyleObj">
     <div class="like-icon" @click="updateLikeInfo" :style="iconStyleObj">
       <div v-if="isLiked">
-        <i class="fas fa-heart full-heart" :style="{ color: '#f03e3e', textShadow: '1px 1px 2px #868e96' }"></i>
+        <i
+          class="fas fa-heart full-heart"
+          :style="{ color: '#f03e3e', textShadow: '1px 1px 2px #868e96' }"
+        ></i>
       </div>
       <div v-else>
         <i v-if="styleType === 2" class="far fa-heart"></i>
@@ -14,7 +17,7 @@
 </template>
 
 <script>
-import * as likeApi from '@/api/like';
+import * as likeApi from "@/api/like";
 
 export default {
   data() {
@@ -37,7 +40,7 @@ export default {
       try {
         let response = null;
 
-        if (this.targetType === 'post') {
+        if (this.targetType === "post") {
           // 게시물 좋아요 수 조회
           response = await likeApi.getPostLike(this.targetId);
         } else {
@@ -53,13 +56,20 @@ export default {
           this.isLiked = false;
         }
 
-        this.numOfLike = response.data.likeCount !== null ? response.data.likeCount : 0;
+        this.numOfLike =
+          response.data.likeCount !== null ? response.data.likeCount : 0;
       } catch (error) {
         console.log(error);
       }
     },
     /* 좋아요 정보 변경하는 함수 */
     updateLikeInfo() {
+      // 만약 로그인 정보가 없다면 로그인 모달 열기
+      if (!this.$store.state.auth.userInfo) {
+        this.$emit("openLoginModal", null);
+        return;
+      }
+
       try {
         let likeCount = this.numOfLike;
 
@@ -67,28 +77,28 @@ export default {
           // 좋아요 취소
           likeCount -= 1;
           if (likeCount < 0) likeCount = 0;
-          if (this.targetType === 'post') {
+          if (this.targetType === "post") {
             // 게시물 좋아요 삭제
             likeApi.deletePostLiked(this.targetId);
-            console.log('delete like of post');
+            console.log("delete like of post");
           } else {
             // this.targetType === 'comment'
             // 댓글 좋아요 삭제
             likeApi.deleteCommentLiked(this.targetId);
-            console.log('delete like of comment');
+            console.log("delete like of comment");
           }
         } else {
           // 좋아요 추가
           likeCount += 1;
-          if (this.targetType === 'post') {
+          if (this.targetType === "post") {
             // 게시물 좋아요 추가
             likeApi.addPostLiked(this.targetId);
-            console.log('add like of post');
+            console.log("add like of post");
           } else {
             // this.targetType === 'comment'
             // 댓글 좋아요 추가
             likeApi.addCommentLike(this.targetId);
-            console.log('add like of comment');
+            console.log("add like of comment");
           }
         }
 
@@ -103,15 +113,15 @@ export default {
     boxStyleObj() {
       if (this.styleType === 0) {
         return {
-          flexDirection: 'column',
+          flexDirection: "column",
         };
       } else if (this.styleType === 1) {
         return {
-          flexDirection: 'row',
+          flexDirection: "row",
         };
       } else if (this.styleType === 2) {
         return {
-          flexDirection: 'column',
+          flexDirection: "column",
         };
       } else {
         return {};
@@ -120,34 +130,34 @@ export default {
     iconStyleObj() {
       if (this.styleType === 0) {
         return {
-          width: '50px',
-          height: '50px',
-          marginTop: '8px',
-          paddingTop: '4px',
-          fontSize: '1.5rem',
-          textAlign: 'center',
-          overflow: 'hidden',
-          background: '#f1f1f1',
-          borderRadius: '100%',
+          width: "50px",
+          height: "50px",
+          marginTop: "8px",
+          paddingTop: "4px",
+          fontSize: "1.5rem",
+          textAlign: "center",
+          overflow: "hidden",
+          background: "#f1f1f1",
+          borderRadius: "100%",
         };
       } else if (this.styleType === 1) {
         return {
-          width: '32px',
-          height: '32px',
-          paddingTop: '4px',
-          marginRight: '6px',
-          fontSize: '1.25rem',
-          textAlign: 'center',
-          overflow: 'hidden',
-          background: '#f1f1f1',
-          borderRadius: '100%',
+          width: "32px",
+          height: "32px",
+          paddingTop: "4px",
+          marginRight: "6px",
+          fontSize: "1.25rem",
+          textAlign: "center",
+          overflow: "hidden",
+          background: "#f1f1f1",
+          borderRadius: "100%",
         };
       } else if (this.styleType === 2) {
         return {
-          width: '32px',
-          height: '32px',
-          fontSize: '1.25rem',
-          textAlign: 'center',
+          width: "32px",
+          height: "32px",
+          fontSize: "1.25rem",
+          textAlign: "center",
         };
       } else {
         return {};
@@ -156,22 +166,22 @@ export default {
     textStyleObj() {
       if (this.styleType === 0) {
         return {
-          width: '100%',
-          display: 'inline-block',
-          marginTop: '4px',
-          fontSize: '0.875rem',
-          lineHeight: '17px',
-          padding: '0 7px 0 8px',
+          width: "100%",
+          display: "inline-block",
+          marginTop: "4px",
+          fontSize: "0.875rem",
+          lineHeight: "17px",
+          padding: "0 7px 0 8px",
         };
       } else if (this.styleType === 1) {
         return {};
       } else if (this.styleType === 2) {
         return {
-          width: '100%',
-          display: 'inline-block',
-          fontSize: '0.875rem',
-          lineHeight: '17px',
-          padding: '0 7px 0 8px',
+          width: "100%",
+          display: "inline-block",
+          fontSize: "0.875rem",
+          lineHeight: "17px",
+          padding: "0 7px 0 8px",
         };
       } else {
         return {};
